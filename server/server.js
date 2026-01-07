8caf0db81fb1422a429d2a0d7dc765d00f96a94d
khalidnbg <khalidnabgaoui4@gmail.com>
2026-01-08 00:27:29 +0100

    setup and connect mongodb

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import connectDB from "./configs/db.js";
 
 // Initialize express app
 const app = express();
+
+// connect db
+await connectDB();
 
 // Middleware
 app.use(cors());
