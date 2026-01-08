af49e92f82de874eb50b66419e9e0f0584769f1a
khalidnbg <khalidnabgaoui4@gmail.com>
2026-01-08 16:02:12 +0100

    feat: add image upload functionality and integrate ImageKit
    - Added multer middleware for handling file uploads.
    - Created a new route for adding cars with image upload.
    - Integrated ImageKit for image management.
    - Updated package.json and package-lock.json to include new dependencies (imagekit and multer).

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,10 +1,50 @@
+import imagekit from "../configs/imageKit.js";
 import User from "../models/User.js";
+import fs from "fs";
+import Car from "../models/Car.js";
 
+// Change user role to owner
 export const changeRoleToOwner = async (req, res) => {
   try {
     const { _id } = req.user;
     await User.findByIdAndUpdate(_id, { role: "owner" });
     res.json({ success: true, message: "Now you can list cars" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// list a car
+export const addCar = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    let car = JSON.parse(req.body.carData);
+    const imageFile = req.file;
+
+    // upload image to imageKit
+    const fileBuffer = fs.readFileSync(imageFile.path);
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/cars",
+    });
+
+    // optimization through imagekit URL transformation
+    var optimizedImageUrl = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        { width: "1280" }, // width resizing
+        { quality: "auto" }, // auto compression
+        { format: "webp" }, // convert to modern format
+      ],
+    });
+
+    const image = optimizedImageUrl;
+
+    await Car.create({ ...car, owner: _id, image });
+
+    res.json({ success: true, message: "Car added successfully" });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
