e8191a566587238426ef60273845566d563e4fdf
khalidnbg <khalidnabgaoui4@gmail.com>
2026-01-10 20:57:40 +0100

    feat: implement booking creation and management APIs in booking controller

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -41,3 +41,102 @@ export const checkAvailabilityOfCars = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// api to create booking
+export const createBooking = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { car, pickupDate, returnDate } = req.body;
+
+    const isAvailable = await checkAvailability(car, pickupDate, returnDate);
+
+    if (!isAvailable) {
+      return res.json({
+        success: false,
+        message: "Car not available for this date",
+      });
+    }
+
+    const carData = await Car.findById(car);
+
+    // calculate price based on pickupDate and return date
+    const picked = new Date(pickupDate);
+    const returned = new Date(returnDate);
+
+    const noOfDays = Math.ceil((returned - picked) / (1000 * 60 * 60 * 24));
+    const price = carData.pricePerDay * noOfDays;
+
+    await Booking.create({
+      car,
+      owner: carData.owner,
+      user: _id,
+      pickupDate,
+      returnDate,
+      price,
+    });
+
+    res.json({ success: true, message: "Booking created successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// api to list user bookings
+export const getUserBookings = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const bookings = await Booking.find({ user: _id })
+      .populate("car")
+      .sort({ createdAt: -1 });
+    res.json({ success: true, bookings });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// api to list owner bookings
+export const getOwnerBookings = async (req, res) => {
+  try {
+    if (req.user.role != "owner") {
+      return res.json({
+        success: false,
+        message: "You are not authorized to perform this action",
+      });
+    }
+
+    const bookings = await Booking.find({ owner: req.user._id })
+      .populate("car", "user")
+      .select("-user.password")
+      .sort({ createdAt: -1 })
+      .res.json({ success: true, bookings });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// api to change booking status
+export const changeBookingStatus = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { bookingId, status } = req.body;
+
+    const booking = await Booking.findById(bookingId);
+
+    if (booking.owner.toString() !== _id.toString()) {
+      return res.json({
+        success: false,
+        message: "You are not authorized to perform this action",
+      });
+    }
+
+    booking.status = status;
+    await booking.save();
+    res.json({ success: true, message: "Booking status updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
