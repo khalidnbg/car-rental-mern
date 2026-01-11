4263d71c9193050b6721a6bf06f3af3bffdbbbf9
khalidnbg <khalidnabgaoui4@gmail.com>
2026-01-11 13:42:07 +0100

    feat: add API to update user image with image upload and optimization

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -168,3 +168,39 @@ export const getDashboardData = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// api to update user image
+export const updateUserImage = async (req, res) => {
+  try {
+    const { _id } = req.user;
+
+    const imageFile = req.file;
+
+    // upload image to imageKit
+    const fileBuffer = fs.readFileSync(imageFile.path);
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/users",
+    });
+
+    // optimization through imagekit URL transformation
+    var optimizedImageUrl = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        { width: "400" }, // width resizing
+        { quality: "auto" }, // auto compression
+        { format: "webp" }, // convert to modern format
+      ],
+    });
+
+    const image = optimizedImageUrl;
+
+    await User.findByIdAndUpdate(_id, { image });
+
+    res.json({ success: true, message: "Image updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
