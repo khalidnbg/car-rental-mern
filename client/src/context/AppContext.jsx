fbc248cef28bd7d1a4fad342435c655a497f861e
khalidnbg <khalidnabgaoui4@gmail.com>
2026-01-11 15:09:05 +0100

    feat: enhance AppContext with user authentication and car fetching functionality

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,9 +1,98 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const value = {};
+  const navigate = useNavigate();
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
+  const [isOwner, setIsOwner] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+
+  const [pickupDate, setPickupDate] = useState("");
+  const [returnDate, setReturnDate] = useState("");
+
+  const [cars, setCars] = useState([]);
+
+  // function to check if user is logged in
+  const fetchUser = async () => {
+    try {
+      const { data } = await axios.get("/api/user/data");
+      if (data.success) {
+        setUser(data.user);
+        setIsOwner(data.user.role === "owner");
+      } else {
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // function to fetch all cars from the server
+  const fetchCars = async () => {
+    try {
+      const { data } = await axios.get("/api/user/cars");
+      data.success ? setCars(data.cars) : toast.error(data.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // logout the user
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+    setUser(null);
+    setIsOwner(false);
+    axios.defaults.headers.common["Authorization"] = "";
+    toast.success("You have been logged out");
+  };
+
+  // retrieve the token from localstorage
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setToken(token);
+    fetchCars();
+  }, []);
+
+  // fetch user data when token is available
+  useEffect(() => {
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = `${token}`;
+      fetchUser();
+    }
+  }, []);
+
+  const value = {
+    navigate,
+    currency,
+    axios,
+    token,
+    setToken,
+    user,
+    setUser,
+    isOwner,
+    setIsOwner,
+    fetchUser,
+    fetchCars,
+    logout,
+    showLogin,
+    setShowLogin,
+    cars,
+    setCars,
+    pickupDate,
+    setPickupDate,
+    returnDate,
+    setReturnDate,
+  };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
